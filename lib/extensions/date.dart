be61fb92bc8399d9893a4b9d44b5f3c14a2fa80a
Dan Reynolds <me@danreynolds.ca>
2025-02-18 09:25:33 -0500

    bump version

--- lib/extensions/date.dart
+++ lib/extensions/date.dart
@@ -1,7 +1,7 @@
 part of date_tools;
 
 extension DateTimeExtensions on DateTime {
-  /// Copies the Date to local time without shifting the source Date to account for its timezone. 1pm PST becomes
+  /// Copies the Date to local time without shifting the source [DateTime] to account for its timezone. 1pm PST becomes
   /// 1pm EST etc.
   DateTime toLocalUnshifted() {
     return DateTime(
